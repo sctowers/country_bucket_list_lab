11e4e108f2eebff20eea02f37bebedae362e1be0
Suzi Towers <suzitowers@gmail.com>
2023-12-04 15:03:38 +0000

    added function to show more details of each country

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const Country = ({country, visited, countryDetails}) => {
+const Country = ({country, visited }) => {
     const [details, setDetails] = useState(false)
 
     const showDetails = () => {
@@ -12,7 +12,15 @@ const Country = ({country, visited, countryDetails}) => {
             {country.name.common}
             <button onClick={visited}>Visited?</button>
             <button onClick={showDetails}>Show Country details</button>
-            {details}
+            {details && (
+                <div>
+                    <h3>Details:</h3>
+                    <p>Capital: {country.capital}</p>
+                    <p>Languages: {country.languages.eng}</p>
+                    <p>Population: {country.population}</p>
+                    <img src={country.flag} alt={`Flag of ${country.name.common}`}/>
+                </div>
+            )}
         </li>
     );
 }
